f307c5e451b5818eb2582d23fe8737d4cd33846d
SerjUstin <s.ustin.92@mail.ru>
2025-12-08 16:46:44 +0300

    Initial commit

---
+++ src/main/java/ru/covenant/code/landing/CovenantCodeLandingServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.covenant.code.landing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovenantCodeLandingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovenantCodeLandingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/covenant/code/landing/CovenantCodeLandingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.covenant.code.landing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovenantCodeLandingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
